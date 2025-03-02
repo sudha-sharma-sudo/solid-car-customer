793187bcfcdb83d9cb00f4cf6b40f4a022364eb1
CodeSandbox <support@codesandbox.io>
2025-03-02 15:33:00 +0000

    Auto-sync commit at 2025-03-02 15:33:00

--- backend/config/database.js
+++ backend/config/database.js
@@ -11,7 +11,12 @@ const connectDB = async () => {
             autoIndex: true,
             serverSelectionTimeoutMS: 5000, // Timeout after 5s instead of 30s
             socketTimeoutMS: 45000, // Close sockets after 45s of inactivity
-            family: 4 // Use IPv4, skip trying IPv6
+            family: 4, // Use IPv4, skip trying IPv6
+            serverApi: {
+                version: '1',
+                strict: true,
+                deprecationErrors: true,
+            }
         };
 
         // Connect to MongoDB
